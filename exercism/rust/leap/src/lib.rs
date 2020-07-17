ce1a15c19ccc20ff21a67ccb0370ed26bcef312b
andy5995 <andy400-dev@yahoo.com>
2020-07-16 23:48:49 -0500

    add leap

---
+++ exercism/rust/leap/src/lib.rs
@@ -0,0 +1,7 @@
+pub fn is_leap_year(year: i32) -> bool {
+    (year % 4 == 0 && year % 100 != 0) || year % 400 == 0
+}
+
+// on every year that is evenly divisible by 4
+// except every year that is evenly divisible by 100
+//    unless the year is also evenly divisible by 400
