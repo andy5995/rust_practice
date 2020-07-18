b9455f3b28506e997f3bb16f9559f7ebaa6b1629
andy5995 <andy400-dev@yahoo.com>
2020-07-18 01:05:58 -0500

    add string_replace.rs

---
+++ snippets/string_replace.rs
@@ -0,0 +1,12 @@
+fn main() {
+ 
+let str1 = "Hello";
+
+let replaced = str1.replace("l", "o, World");
+println!("{}", replaced);
+// Outputs: Heo, Worldo, Worldo
+
+let replaced = str1.replace("o", "o, World");
+println!("{}", replaced);
+
+}
